c77ff220b27db7a2ce89e2e3459bc9416894ab7d
Stone <axelstoun@mail.ru>
2018-04-10 17:12:50 +0300

    Почти готов frontend

--- js/custom.js
+++ js/custom.js
@@ -17,7 +17,92 @@
                 
             }
         }
-        
-        
+
+        //Активируем фото-карусель
+        $('.photo').owlCarousel({
+            center: true,
+            items:2,
+            loop:true,
+            margin:10,
+            responsive:{
+                600:{
+                    items:4
+                }
+            }
+        });
+
+        //Функция смены шагов
+        selectBottomStep();
+        selectBottomStepMobile()
+        function selectBottomStepMobile() {
+            $("#mobile-select").change(function(){
+                showHideBlock($(this).val())
+            });
+        }
+        function selectBottomStep() {
+            var select = $(".pc-step a");
+            var selectBottom = $('.step-buttons-bottom a');
+            select.click(function (e) {
+                e.preventDefault();
+                var bottomCount =  select.length;
+                var step = $(this).attr('id');
+                for(var i = 0; bottomCount > i;i++)
+                {
+                    if($(select[i]).attr('id') == step)
+                    {
+                        $(select[i]).addClass("active");
+                        $(selectBottom [i]).addClass("active");
+                        showHideBlock(step);
+                    }else
+                    {
+                        if($(select[i]).hasClass("active"))
+                        {
+                            $(select[i]).removeClass("active");
+                            $(selectBottom[i]).removeClass("active");
+                        }
+                    }
+                }
+            });
+
+            selectBottom.click(function (e) {
+                e.preventDefault();
+                /*
+                var bottomCount =  selectBottom.length;
+                var step = $(this).attr('id');
+                for(var i = 0; bottomCount > i;i++)
+                {
+                    if($(selectBottom[i]).attr('id') == step)
+                    {
+                        $(select[i]).addClass("active");
+                        $(selectBottom [i]).addClass("active");
+                        showHideBlock(step);
+                    }else
+                    {
+                        if($(selectBottom[i]).hasClass("active"))
+                        {
+                            $(select[i]).removeClass("active");
+                            $(selectBottom[i]).removeClass("active");
+                        }
+                    }
+                }
+                 */
+            })
+
+        }
+        function showHideBlock(e) {
+            var select = $('.step-block-content > div');
+            var count = select.length;
+            for(var i = 0 ;count > i ; i++)
+            {
+                if($(select[i]).attr('class') == e)
+                {
+                    $(select[i]).fadeIn('slow')
+                }else
+                {
+                    $(select[i]).fadeOut()
+                }
+            }
+        }
+
     })
 })();
