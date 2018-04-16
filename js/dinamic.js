a4287d5391621bde9e694c13e441861e1d50b5fa
Алексей Буховский <ab@may.office.zanevsky>
2018-04-16 16:47:56 +0300

    Цок готов
    ned

--- js/dinamic.js
+++ js/dinamic.js
@@ -9,9 +9,9 @@
             var CommentCount = $(".reviews .carousel-item").length;
             for (var i = 0; CommentCount > i; ++i) {
                 if (i == 0) {
-                    $('#reviews-controller').append('<li data-target="#reviewsIndicators" data-slide-to="' + i + '" class="active"></li>');
+                    $('#reviews-controller').append('<li data-target="#comment-carousel" data-slide-to="' + i + '" class="active"></li>');
                 } else {
-                    $('#reviews-controller').append('<li data-target="#reviewsIndicators" data-slide-to="' + i + '"></li>');
+                    $('#reviews-controller').append('<li data-target="#comment-carousel" data-slide-to="' + i + '"></li>');
                 }
 
             }
